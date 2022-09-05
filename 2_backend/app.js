c35bc7d7cb702d3859be9f67c6bdaed3278d08ef
Skaiste <smacaityte@gmail.com>
2022-09-05 21:45:32 +0300

    Updated app file

--- 2_backend/app.js
+++ 2_backend/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-const asyncHandler = require('express-async-handler');
 require('dotenv').config();
 
+const {
+  getAllClients,
+  addClient,
+  updateClient,
+  deleteClient,
+} = require('./controller/client.controller');
+
 const connectMongoDB = require('./config/db.js');
-const User = require('./models/User.model.js');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 connectMongoDB();
@@ -14,11 +22,17 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.get(
-  '/api/clients',
-  asyncHandler(async (req, res) => {
-    res.json('logic is not implemented yet');
-  })
-);
+
+// GET - all clients data
+app.get('/api/clients', getAllClients);
+
+// POST - new client
+app.post('/api/clients', addClient);
+
+// PUT - update client by id
+app.put('/api/clients/:id', updateClient);
+
+// DELETE - client by id
+app.delete('/api/clients/:id', deleteClient);
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
