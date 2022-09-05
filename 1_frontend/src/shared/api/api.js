bc85d6e073c8752a9f0a4125575474046e8b9ccf
Skaiste <smacaityte@gmail.com>
2022-09-05 22:17:23 +0300

    Created api and added logic

---
+++ 1_frontend/src/shared/api/api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const HOST = 'http://localhost:5000';
+
+class API {
+  async getData() {
+    const { data } = await axios.get(`${HOST}/api/clients`);
+
+    return data;
+  }
+  async addData(clientData) {
+    const { data } = await axios.post(`${HOST}/api/clients`, clientData);
+
+    return data;
+  }
+  async updateData(id, clientData) {
+    const { data } = await axios.get(`${HOST}/api/clients/${id}`, clientData);
+
+    return data;
+  }
+  async deleteData() {
+    const { data } = await axios.get(`${HOST}/api/clients/${id}`);
+
+    return data;
+  }
+}
+
+const api = new API();
+
+export default api;
