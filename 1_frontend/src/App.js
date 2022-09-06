9cb5c420681e6d0ddb1dd234969dd3e9811381b9
Skaiste <smacaityte@gmail.com>
2022-09-06 21:23:58 +0300

    Added lazy import logic, Routes, Suspense

--- 1_frontend/src/App.js
+++ 1_frontend/src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+const HomePage = lazy(() => import('./pages/HomePage'));
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<p>Loading..</p>}>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 }
 
 export default App;
