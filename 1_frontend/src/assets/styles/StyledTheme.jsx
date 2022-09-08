9677b0aaa326d946d205f724e277a045eafb9935
Skaiste <smacaityte@gmail.com>
2022-09-08 19:58:16 +0300

    Created theme

---
+++ 1_frontend/src/assets/styles/StyledTheme.jsx
@@ -0,0 +1,15 @@
+const theme = {
+  color: {
+    green: '#4d724d',
+    red: '#D81C23',
+    orange: '#d29849',
+    darkOrange: '#d97441',
+    white: '#f5f5f5',
+    black: '#212221',
+    grey: '#727171',
+    btnBgTransWhite: '#ffffffdf',
+    bgTransWhite: 'rgba(251, 251, 250, 0.849)',
+  },
+};
+
+export default theme;
