cf522b3f8985305da6dcb930bf896bc5fbfe4557
Skaiste <smacaityte@gmail.com>
2022-09-08 20:10:17 +0300

    Added Time availability logic to client controller

--- 2_backend/controller/client.controller.js
+++ 2_backend/controller/client.controller.js
@@ -2,24 +2,27 @@ const MESSAGES = require('../shared/constants');
 const Client = require('../models/Client.model.js');
 const asyncHandler = require('express-async-handler');
 
-const getAllClients = asyncHandler(async (req, res) => {
+const getClientsData = asyncHandler(async (req, res) => {
   // Find All data
   const clients = await Client.find();
 
+  const data = {};
+
+  // Get pages amount
+  data.pages = Math.ceil(clients.length / 10);
+
+  // Reverse to show data from new to older
   const reversedClients = [...clients].reverse();
 
   // Set amount of data
   const amount = 10;
-
   // Set page
   let page = req.params.page;
-
   // Set start/end index by page
   const startIndex = (page - 1) * amount;
   const endIndex = page * amount;
-
   // Get limited data
-  const data = reversedClients.slice(startIndex, endIndex);
+  data.limitedClientsData = reversedClients.slice(startIndex, endIndex);
 
   if (data) {
     res.send(data);
@@ -33,6 +36,23 @@ const addClient = asyncHandler(async (req, res) => {
   const { name, email, date, time } = req.body;
 
   // Checking time availability
+  const isNotAvailable = await Client.findOne({ time, date });
+
+  if (isNotAvailable) {
+    res.status(400).send({
+      message: 'Appointment time is not available',
+    });
+    throw new Error({ message: 'Appointment time is not available' });
+  }
+
+  if (!name || !email) {
+    res.status(400).send({
+      message: `Registration failed, check name or email`,
+    });
+    throw new Error({
+      message: `Registration failed, check name or email`,
+    });
+  }
 
   const client = await Client.create({ name, email, date, time });
 
@@ -47,6 +67,19 @@ const addClient = asyncHandler(async (req, res) => {
 const updateClient = asyncHandler(async (req, res) => {
   const id = req.params.id;
   const newData = req.body;
+
+  const time = newData.time;
+  const date = newData.date;
+
+  // Checking time availability
+  const isNotAvailable = await Client.findOne({ time, date });
+
+  if (isNotAvailable) {
+    res.status(400).send({
+      message: 'Appointment time is not available',
+    });
+    throw new Error({ message: 'Appointment time is not available' });
+  }
 
   const updatedClient = await Client.findByIdAndUpdate(id, newData);
 
@@ -70,7 +103,7 @@ const deleteClient = asyncHandler(async (req, res) => {
 });
 
 module.exports = {
-  getAllClients,
+  getClientsData,
   addClient,
   updateClient,
   deleteClient,
