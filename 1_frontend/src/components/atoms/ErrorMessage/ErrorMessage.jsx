c1fe9a4c3b2456b58c43933b0e8735ab238db581
Skaiste <smacaityte@gmail.com>
2022-09-08 22:13:30 +0300

    Removed setTimeout from ErrorMessage

--- 1_frontend/src/components/atoms/ErrorMessage/ErrorMessage.jsx
+++ 1_frontend/src/components/atoms/ErrorMessage/ErrorMessage.jsx
@@ -8,14 +8,10 @@ const ErrorMessage = ({ text }) => {
     if (text) {
       setErrorMessage(text);
     }
-
-    setTimeout(() => {
-      setErrorMessage('');
-    }, 6000);
   }, [text]);
 
   const messageColor = () => {
-    if (errorMessage.includes('not') || errorMessage.includes('failed')) {
+    if (errorMessage.includes('not') || errorMessage.includes('deleted')) {
       return '#D81C23';
     } else {
       return '#4d724d';
