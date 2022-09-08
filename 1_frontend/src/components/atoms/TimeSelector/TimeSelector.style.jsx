de40ee00a68bcd2b0c0f407bec8e60aad89f3c65
Skaiste <smacaityte@gmail.com>
2022-09-08 20:01:17 +0300

    Updated time select style

--- 1_frontend/src/components/atoms/TimeSelector/TimeSelector.style.jsx
+++ 1_frontend/src/components/atoms/TimeSelector/TimeSelector.style.jsx
@@ -2,9 +2,24 @@ import styled from 'styled-components';
 
 export const StyledSelectDiv = styled.div`
   select {
-    padding: 5px;
+    padding: 8px;
     font-size: 16px;
+
+    border: none;
+
+    background-color: ${({ theme }) => theme.color.btnBgTransWhite};
+
+    cursor: pointer;
+
+    option {
+      cursor: pointer;
+
+      &:hover {
+        background-color: green;
+      }
+    }
   }
+
   span {
     font-size: 16px;
     margin: 0px 5px;
