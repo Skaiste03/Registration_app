916099b0e851e688dc201ac52fba0e43a2ec005f
Skaiste <smacaityte@gmail.com>
2022-09-08 20:08:22 +0300

    Added HomePage Wrapper style Updated Section style

--- 1_frontend/src/pages/HomePage/HomePage.style.jsx
+++ 1_frontend/src/pages/HomePage/HomePage.style.jsx
@@ -1,19 +1,24 @@
 import styled from 'styled-components';
+import image from '../../assets/images/bg-img.jpg';
 
 export const StyledContainer = styled.div`
-  max-width: 1000px;
+  max-width: 1200px;
   margin: 0 auto;
 `;
 
-export const StyledFormHeadline = styled.h3`
-  font-size: 24px;
-  margin: 15px;
-`;
-
-export const StyledFormSection = styled.section`
+export const StyledSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  padding: 10px 0px 50px 0px;
+  padding: 20px 0px 40px 0px;
+`;
+
+export const StyledWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+
+  background-image: url(${image});
+  background-position: 50% 50%;
+  background-size: cover;
 `;
