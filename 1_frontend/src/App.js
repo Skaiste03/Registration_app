c5f56ecfd1520c2fd145f3f4f3d7ab908da50f64
Skaiste <smacaityte@gmail.com>
2022-09-08 19:57:12 +0300

    Added ThemeProvider and Loader component into App

--- 1_frontend/src/App.js
+++ 1_frontend/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Loader from './components/atoms/Loader';
+import { ThemeProvider } from 'styled-components';
+import theme from './assets/styles/StyledTheme';
+
 const HomePage = lazy(() => import('./pages/HomePage'));
 
 function App() {
   return (
-    <BrowserRouter>
-      <Suspense fallback={<p>Loading..</p>}>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
