5740f9b19ad8ddd49f6d13eb078a1196e465004b
Skaiste <smacaityte@gmail.com>
2022-09-08 20:06:01 +0300

    Updated List style

--- 1_frontend/src/components/organisms/List/List.style.jsx
+++ 1_frontend/src/components/organisms/List/List.style.jsx
@@ -4,13 +4,28 @@ export const StyledTableContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  align-items: flex-start;
+  overflow-x: auto;
+
+  background: ${({ theme }) => theme.color.bgTransWhite};
+
+  padding: 30px;
+  margin: 10px;
+
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+
+  @media screen and (min-width: 1100px) {
+    border-radius: 10px;
+    align-items: center;
+  }
 `;
 
 export const StyledHeadline = styled.div`
-  width: 100%;
+  display: inline-block;
+  min-width: 100%;
   text-align: center;
-  margin-bottom: 10px;
+  margin-bottom: 20px;
+  color: ${({ theme }) => theme.color.grey};
 
   h3 {
     font-size: 24px;
@@ -26,28 +41,43 @@ export const StyledTable = styled.table`
   width: 100%;
   border-collapse: collapse;
 
-  td,
+  td {
+    padding: 5px 0px;
+    text-align: center;
+  }
+
   th {
-    border: 1px solid #ddd;
-    padding: 8px;
+    padding: 15px 0px;
   }
 
   td input {
+    text-align: center;
+    font-size: 14px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.color.black};
+
     border: none;
-    padding: 0;
+
+    padding: 10px 0px;
+    background-color: ${({ theme }) => theme.color.btnBgTransWhite};
+    cursor: pointer;
   }
 
   td input::placeholder {
     font-size: 16px;
-    color: black;
+    color: ${({ theme }) => theme.color.black};
   }
 
   td input:focus {
     outline: none;
+    background-color: ${({ theme }) => theme.color.orange};
+    color: ${({ theme }) => theme.color.white};
+
+    cursor: text;
   }
 
   thead {
-    background-color: #4ac6d2;
-    color: #fff;
+    background-color: ${({ theme }) => theme.color.green};
+    color: ${({ theme }) => theme.color.white};
   }
 `;
