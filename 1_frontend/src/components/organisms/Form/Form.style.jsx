79fa12964b2087e9bd41bb9c75620c11ef8265e4
Skaiste <smacaityte@gmail.com>
2022-09-08 20:03:26 +0300

    Updated Form component style

--- 1_frontend/src/components/organisms/Form/Form.style.jsx
+++ 1_frontend/src/components/organisms/Form/Form.style.jsx
@@ -1,5 +1,24 @@
 import styled from 'styled-components';
 
+export const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  padding: 30px;
+
+  border-radius: 10px;
+
+  background: ${({ theme }) => theme.color.bgTransWhite};
+`;
+
+export const StyledHeadline = styled.h3`
+  font-size: 20px;
+  margin-bottom: 15px;
+
+  color: ${({ theme }) => theme.color.grey};
+`;
+
 export const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
@@ -13,12 +32,31 @@ export const StyledInputDiv = styled.div`
   flex-direction: column;
 
   .react-datetime-picker__wrapper {
+    border: none;
     padding: 5px;
+
+    background-color: ${({ theme }) => theme.color.btnBgTransWhite};
+    input {
+      cursor: pointer;
+    }
   }
 
   #email,
   #name {
-    padding: 10px 5px;
+    padding: 10px;
+    background-color: ${({ theme }) => theme.color.btnBgTransWhite};
+    border: none;
+
+    font-size: 16px;
+    font-weight: 400;
+
+    cursor: pointer;
+  }
+
+  #email:focus,
+  #name:focus,
+  .react-datetime-picker__wrapper input:focus {
+    cursor: text;
   }
 
   #submit {
@@ -26,18 +64,18 @@ export const StyledInputDiv = styled.div`
     font-size: 16px;
     font-weight: 500;
     border: none;
-    background-color: #9e15bf;
-    color: #ffff;
+    background-color: ${({ theme }) => theme.color.orange};
+    color: ${({ theme }) => theme.color.white};
+
+    border-radius: 10px;
+    border: 1px solid ${({ theme }) => theme.color.orange};
 
     cursor: pointer;
-    transition: 0.3s ease-in-out;
+    transition: 0.6s ease-in-out;
 
     &:hover {
-      opacity: 0.9;
+      background-color: ${({ theme }) => theme.color.btnBgTransWhite};
+      color: ${({ theme }) => theme.color.orange};
     }
   }
-`;
-
-export const StyledMessage = styled.p`
-  color: ${(props) => props.textColor};
 `;
